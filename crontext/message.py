9f21f518d9ddef8d24711d0b301ae41c801db5ce
Hugh O'Reilly <horeilly1101@gmail.com>
2019-06-28 01:11:37 -0500

    updated requirements

--- crontext/message.py
+++ crontext/message.py
@@ -2,8 +2,18 @@
 
 
 class Message:
-	def __init__(self, text):
-		self.text = text
+	def __init__(self, storage):
+		self.storage = storage
 
 	def __str__(self):
-		return self.text
+		return "{0}(storage={1})".format(self.__name__, self.storage)
+
+
+class TextMessage(Message):
+	def __init__(self, text):
+		super().__init__(text)
+
+
+class RescheduleMessage(Message):
+	def __init__(self, time):
+		super().__init__(time)
