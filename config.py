12907d4249accfa1ac8b664bdacc49f82dccc606
Hugh O'Reilly <horeilly1101@gmail.com>
2019-07-02 00:53:39 -0500

    uncommented some things

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ class ServerConfig:
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-# class WorkerConfig:
-# 	"""Object to keep track of config variables for the worker module."""
-# 	# Your Account Sid and Auth Token from twilio.com/user/account
-# 	ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
-# 	AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
-#
-# 	# the phone number sending the text message (should be associated with your twilio
-# 	# account)
-# 	FROM_NUMBER = os.environ['TWILIO_FROM_NUMBER']
-#
-# 	# the phone number receiving the text
-# 	TO_NUMBER = os.environ['TWILIO_TO_NUMBER']
+class WorkerConfig:
+	"""Object to keep track of config variables for the worker module."""
+	# Your Account Sid and Auth Token from twilio.com/user/account
+	ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+	AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+
+	# the phone number sending the text message (should be associated with your twilio
+	# account)
+	FROM_NUMBER = os.environ['TWILIO_FROM_NUMBER']
+
+	# the phone number receiving the text
+	TO_NUMBER = os.environ['TWILIO_TO_NUMBER']
