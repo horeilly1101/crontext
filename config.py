9b5a94804fc178a6259845eef224b62f130a9922
Hugh O'Reilly <horeilly1101@gmail.com>
2019-07-01 22:40:31 -0500

    another stab at heroku

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 
 class ServerConfig:
 	"""Object to keep track of config variables for the server module."""
-	# SECRET_KEY = os.environ['SECRET_KEY']
-	SECRET_KEY = ""
+	SECRET_KEY = os.environ['SECRET_KEY']
+	# SECRET_KEY = ""
 
 	# configure the database
 	# SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
